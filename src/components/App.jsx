e3a996903b72dca2e6d55a5f59e3112dfdbdb57d
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-09 16:32:38 -0800

    Render VideoList and VideoPlayer components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,50 @@
-var App = ({video}) => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer />
-    </div>
-    <div className="col-md-5">
-      <VideoList />
-    </div>
-  </div>
-);
+// var App = ({video}) => (
+//   <div>
+//     <Nav />
+//     <div className="col-md-7">
+//       <VideoPlayer video={video} />
+//     </div>
+//     <div className="col-md-5">
+//       <VideoList />
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-//     this.state = {
-//       videos: []
-//     }
-//   }
+    this.state = {
+      videos: window.exampleVideoData,
+      currentVideo: window.exampleVideoData[0]
+    }
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <Nav />
-//         <div className="col-md-7">
-//           <VideoPlayer video={video} />
-//         </div>
-//         <div className="col-md-5">
-//           <VideoList videos={videos} />
-//         </div>
-//       </div>
-//     );
-//   }
-// };
+  videoOnClick(video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+       <Nav />
+       <div className="col-md-7">
+         <VideoPlayer video={this.state.currentVideo} />
+       </div>
+       <div className="col-md-5">
+         <VideoList
+          videos={this.state.videos}
+          videoOnClick={this.videoOnClick.bind(this)}
+        />
+       </div>
+     </div>
+    )
+  }
+};
 
 window.App = App;
