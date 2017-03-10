318d8781bb7d741108cada81600053af36a34281
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-09 16:37:04 -0800

    Implement VideoPlayer component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,7 +1,8 @@
 var VideoPlayer = ({video}) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item"
+      src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
       <h3>{video.snippet.title}</h3>
@@ -19,3 +20,24 @@ VideoPlayer.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
+
+/*
+App - The top level container for the entire application.
+This is the component that will be rendered to the DOM
+
+Nav - A container component for the top navigation bar
+
+Search - Responsible for knowing and communicating
+information about the search input field
+
+VideoPlayer - Responsible for playing a single video and
+displaying information about it
+
+VideoList - A container component responsible for
+populating video list entry components
+
+VideoListEntry - A component that shows a thumbnail view
+for a single video. When the title of this video is
+clicked, that video will be played in the VideoPlayer
+component
+*/
